6388a76451a52331f6877c9f6673bce5a9ae893a
Nivetha1626 <nivethadharman06@gmail.com>
2026-06-10 15:46:02 +0530

    program

---
+++ src/day5.java
@@ -0,0 +1,22 @@
+/*void main() {
+  /* program 1
+   String name = "vetias";
+    System.out.println(name.toUpperCase());
+}*/
+
+/*program 2
+System.out.println("Enter your name:");
+        Scanner sc= new Scanner(System.in);
+       String name=sc.nextLine();
+ System.out.println("Enter your department:");
+     String department =sc.nextLine();
+     System.out.println("Enter your age:");
+       String age =sc.next();
+System.out.println("Details");
+       System.out.println("Name: "+name);
+       System.out.println("Department:"+department);
+       System.out.println("Age:"+age);
+
+}
+
+ */
