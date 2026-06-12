77422e301346034bf2a0d9d37cad8450d9d0b3b0
Nivetha1626 <nivethadharman06@gmail.com>
2026-06-12 15:29:47 +0530

    day 3

---
+++ src/day3.java
@@ -0,0 +1,55 @@
+public class day3 {
+    /*void main() {
+    /*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+
+    int n=5;
+    //row loop
+    for (int row =1;row<=n; row++) {
+        //column loop
+        for (int column = 1; column < row; column++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+        }
+           */
+
+    /* char ch = 5;
+     String n = "ABCDE";
+     //row loop
+     for (int row = 0; row < ch; row++) {
+         //column loop
+         for (int column = 0; column <=row; column++) {
+             System.out.print(n.charAt(column));
+         }
+         System.out.println();
+     }
+ }*/
+   /* int[] marks = {87, 32, 44, 89 , 23};
+    System.out.println("pass mark");
+    for(int mark:marks){
+        if(mark>=35){
+            System.out.println(mark+"");
+        }
+    }
+    System.out.println("fail marks");
+    for(int mark:marks){
+        if(mark<35){
+            System.out.println(mark+"");
+        }
+    }
+}/*
+
+
+    */
+    void main(){
+        String name="java program";
+
+        System.out.println(name.toUpperCase());
+    }
+}
