f365f11f92cf4cac0aa84b8bb28f0620823c685d
Nivetha <nivethadharman06@gmail.com>
2026-06-15 15:47:47 +0530

    college

--- src/day9.java
+++ src/day9.java
@@ -1,2 +1,42 @@
-public class day9 {
+static class College implements AutoCloseable {
+    College(){
+        System.out.println("College");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("college Close");
+    }
 }
+
+static class Department extends College {
+    Department(){
+        System.out.println("Department");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Department Close");
+        super.close();
+    }
+}
+
+static class SecondYear extends Department{
+    SecondYear(){
+        System.out.println("SecondYear");
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("SecondYear Close");
+        super.close();
+    }
+}
+
+static void main() throws Exception {
+    try(SecondYear secondYear= new SecondYear())
+    {
+
+    }
+}
+
