281f37acdf16a43b503cd95a8205f339e1dc0eda
Nivetha1626 <nivethadharman06@gmail.com>
2026-06-15 09:32:03 +0530

    Login program

--- src/day8.java
+++ src/day8.java
@@ -12,7 +12,7 @@ public class  day8 {
 
      */
 
-    enum LANGUAGES {
+   /* enum LANGUAGES {
         TA("Tamil"),
         EN("English"),
         GR("German"),
@@ -29,3 +29,46 @@ String description;
         System.out.println(LANGUAGES.GR.description);
     }
 }
+
+    */
+
+
+        sealed interface LoginResult permits LoginSuccess, LoginFailure, LoginLoading {
+
+        }
+
+        record LoginSuccess(String time) implements LoginResult {
+
+        }
+
+        record LoginFailure(String reason) implements LoginResult {
+
+        }
+
+        record LoginLoading(String expectedTime) implements LoginResult {
+
+        }
+
+        record Rendom(String reason) {
+
+        }
+
+        static void handleLogin(LoginResult result) {
+            switch (result) {
+                case LoginSuccess success -> System.out.println("Login Success at :" + success.time());
+                case LoginFailure failure -> System.out.println("Login Failure becasuse of " + failure.reason());
+                case LoginLoading loading -> System.out.println("Login Loadind, " + loading.expectedTime());
+            }
+        }
+
+        static void main() {
+            LoginResult loginSuccess = new LoginSuccess("2026-06-14:10pm");
+            LoginFailure loginFailure = new LoginFailure("Invailed User name or Password");
+            LoginLoading loginLoading = new LoginLoading("just 2 mins");
+
+            handleLogin(loginSuccess);
+            handleLogin(loginFailure);
+            handleLogin(loginLoading);
+        }
+    }
+
