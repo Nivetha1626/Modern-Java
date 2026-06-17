f6813bb10caf7ce7b4ae3f4c427a79b713f40db6
Nivetha <nivethadharman06@gmail.com>
2026-06-17 09:37:52 +0530

    Student

--- src/day10.java
+++ src/day10.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+//import java.util.LinkedList;
 import java.util.Queue;
 
 /*
@@ -23,6 +23,7 @@ public class day10 {
     }
 }
      */
+/*
 public class day10 {
     static void main() {
         LinkedList<String> queue = new LinkedList<String>();
@@ -38,8 +39,63 @@ public class day10 {
     static void process(LinkedList<String> queue){
         String removed=queue.removeFirst();
         System.out.println("Removed:"+removed);
-
     }
 }
+ */
+import java.util.LinkedList;
 
+public class day10 {
+
+    static class Student {
+        String name;
+        int regNo;
+        int marks;
+
+        Student(String name, int regNo, int marks) {
+            this.name = name;
+            this.regNo = regNo;
+            this.marks = marks;
+        }
+    }
+
+    static void removeStudent(LinkedList<Student> students) {
+        System.out.println("Removed : " + students.removeFirst().name);
+    }
+
+    static void updateStudent(LinkedList<Student> students) {
+        for (Student s : students) {
+            if (s.name.equals("Mithra")) {
+                s.marks = 95; // Update marks from 85 to 95
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<Student> students = new LinkedList<>();
+
+        // Add 5 Students
+        students.add(new Student("Nive", 101, 80));
+        students.add(new Student("Narthi", 102, 75));
+        students.add(new Student("Mithra", 103, 90));
+        students.add(new Student("Menaga", 104, 85));
+        students.add(new Student("Sweatha", 105, 88));
+
+        // Remove 1 Student
+        removeStudent(students);
+
+        // Update 1 Student
+        updateStudent(students);
+
+        // Display Students
+        System.out.println("\nStudent Details");
+        for (Student s : students) {
+            System.out.println(
+                    s.name + " " +
+                            s.regNo + " " +
+                            s.marks
+            );
+        }
+    }
+}
 
