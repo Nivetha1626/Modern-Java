1a101e089d5f14a1a910948bf65cca35b1fc55d7
Nivetha <nivethadharman06@gmail.com>
2026-06-17 14:52:55 +0530

    Set

--- src/day11.java
+++ src/day11.java
@@ -1,2 +1,20 @@
-
-
+import java.util.HashSet;
+import java.util.Set;
+public class day11 {
+    public static void main() {
+        Set<String> set=new HashSet<>();
+        set.add("Student 2");
+        for (String s : set) {
+            System.out.println(s);
+        }
+        set.add("Student 1");
+        set.add("Student 2");
+        set.add("Student 1");
+        set.add("Student 1");
+        set.add("Student 2");
+        System.out.println("set :");
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
