80ba6a3751d4008d0a28f7ff30d6039ed33d940c
Nivetha <nivethadharman06@gmail.com>
2026-06-19 15:57:03 +0530

    student csv

--- src/day13.java
+++ src/day13.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+/*
 public class day13 {
     static void main() {
         Path path = Path.of("files", "report.txt");
@@ -14,6 +15,7 @@ public class day13 {
 
             for (String line : data) {
                 System.out.println(line);
+
             }
             newData.add("Student1");
             newData.add("Student2");
@@ -26,3 +28,24 @@ public class day13 {
         }
     }
 }
+
+ */
+
+    static void main() {
+        Path path = Path.of("file", "student.csv");
+        try {
+            List<String> students = Files.readAllLines(path);
+
+            for (String line : students) {
+                String[] row = line.split(",");
+                for (int i = 0; i < row.length; i++) {
+                    System.out.print(row[i] + " | ");
+                }
+                System.out.println();
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+
+
